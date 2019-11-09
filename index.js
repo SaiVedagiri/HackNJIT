91d357b93f06346c6ad6dced51e8111b6c01bf01
Sai Vedagiri <43285226+SaiVedagiri@users.noreply.github.com>
2019-11-09 16:14:19 -0500

    Pushed all code

--- index.js
+++ index.js
@@ -1,63 +1,232 @@
-const express = require('express')
+const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const path = require('path')
-const app = express();
-app.use(express.json());
+var request = require("request");
+var unirest = require("unirest");
+
 const PORT = process.env.PORT || 5000
 const API_KEY = "AIzaSyDhkJ2yT06tRwXIMEUp9xaj2-LxOnKyvGY";
 
+const app = express();
+let state = "default";
+let url1;
+let url2;
+let url3;
+let url4;
+let url5;
+let fromNum;
 
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+  //console.log(req);
+  const accountSid = 'AC61eca8833f419fdc26e5ffa75b284891';
+  const authToken = '91bf82ff6ea981dfc77db8d5cb13ad4a';
+  const client = require('twilio')(accountSid, authToken);
+  client.messages.list({ limit: 1 })
+    .then(messages => checkInput(messages[0].body, messages[0].from, req, res));
+  //twiml.message(myMessage);
+  //console.log(myMessage);
+  //res.writeHead(200, { 'Content-Type': 'text/xml' });
+  //res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
+
+function sendMessage(message, req, res) {
+  const accountSid = 'AC61eca8833f419fdc26e5ffa75b284891';
+  const authToken = '91bf82ff6ea981dfc77db8d5cb13ad4a';
+  const client = require('twilio')(accountSid, authToken);
+
+  client.messages
+    .create({
+      body: ":\n" + message,
+      from: '+12015847119',
+      to: fromNum
+    })
+    .then(message => res.send(message));
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', function (req, res) {
-     res.send(req.body);
+    console.log(typeof (req.params.test));
+    res.send(req);
   })
-  .get('/api', function(req, res) {
+  .get('/api', function (req, res) {
     const accountSid = 'AC61eca8833f419fdc26e5ffa75b284891';
     const authToken = '91bf82ff6ea981dfc77db8d5cb13ad4a';
     const client = require('twilio')(accountSid, authToken);
 
     client.messages
       .create({
-        body: req.body,
+        body: "Hi Josh",
         from: '+12015847119',
-        to: '+18484681542'
+        to: '+19179404729'
       })
-      .then(message => res.send(req.body));
+      .then(message => res.send("Hi Josh"));
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/article', function (req, res) {
+    makeRequestArticle("https://www.cnn.com/2019/11/09/us/las-vegas-abduction-father-daughter-charged/index.html", res);
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-  //get input from Arya
 
-  // if (input.includes("info"))
-  // {
-  //   //call wolfram alpha api with input
-    
-  // }
-
-  // else
-  // {
-    //var URL = 'https://api.wolframalpha.com/v1/result?i=${input}%3F&appid=7PPG9H-H5KLEVRLK2';
-    // $.getJSON(URL, function (data));
-
-  // }
-  /*
-  text = "Hello World"
-  document = types.Document(
-    content=text.encode('utf-8'),
-    type=enums.Document.Type.PLAIN_TEXT)
-
+//get input from Arya
+async function checkInput(input, currentFromNum, req, res) {
+  fromNum = currentFromNum;
+  input = input.trim();
+  input = input.toLowerCase();
+  if (state == "news") {
+    if (input.includes("1"))
     {
-  "document": {
-    object(document)
-  },
-  "encodingType": enum(UTF8)
-}
-  
- 
-  
+      makeRequestArticle(url1, res);
+      state = "default";
 
- POST https://language.googleapis.com/v1beta2/documents:analyzeEntities?key=API_KEY
-*/
+    }
+    else if (input.includes("2"))
+    {
+      makeRequestArticle(url2, res);
+      state = "default";
+
+    }
+    else if (input.includes("3"))
+    {
+      makeRequestArticle(url3, res);
+      state = "default";
+
+    }
+    else if (input.includes("4"))
+    {
+      makeRequestArticle(url4, res);
+      state = "default";
+
+    }
+    else if (input.includes("5"))
+    {
+      console.log(url5);
+      makeRequestArticle(url5, res);
+      state = "default";
+    }
+  }
+  else if (input.includes("info")) {
+    let words = input.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      if (words[i] == "about" || words[i] == "on") {
+        var index = i;
+        console.log("index:" + index);
+        break;
+      }
+    }
+    let search = "";
+    for (i = index + 1; i < words.length; i++) {
+      search += words[i];
+    }
+    makeRequestWikipedia(`https://en.wikipedia.org/api/rest_v1/page/summary/${search}`, req, res);
+  }
+  else if (input.toLowerCase().trim().includes("news")) {
+    makeRequestNews(`https://newsapi.org/v2/top-headlines?country=us&apiKey=9e06d9d66c7440d995c78259238d4e68`, req, res);
+  }
+
+  else if (input.includes("weather")) {
+    let words = input.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      if (words[i] == "in" || words[i] == "on" || words[i] == "for") {
+        var index = i;
+        console.log("index:" + index);
+        break;
+      }
+    }
+    let search = "";
+    for (i = index + 1; i < words.length; i++) {
+      search += words[i];
+    }
+    makeRequestWeather(`https://api.openweathermap.org/data/2.5/weather?q=${search}&apikey=d4aba2f6472ab5c29ac2771336221dd8`, req, res)
+  }
+}
+
+async function makeRequestWikipedia(uri, req, res) {
+  await request({
+    uri: uri,
+    method: "GET",
+    timeout: 10000,
+    followRedirect: true,
+    maxRedirects: 10
+  },
+    async function (error, response, body) {
+      sendMessage(JSON.parse(body).extract, req, res);
+    });
+}
+
+async function makeRequestNews(uri, req, res) {
+  await request({
+    uri: uri,
+    method: "GET",
+    timeout: 10000,
+    followRedirect: true,
+    maxRedirects: 10
+  },
+    async function (error, response, body) {
+      sendMessage("Enter 1 to read " + JSON.parse(body).articles[0].title + 
+      "\n\nEnter 2 to read " + JSON.parse(body).articles[1].title + 
+      "\n\nEnter 3 to read " + JSON.parse(body).articles[2].title + 
+      "\n\nEnter 4 to read " + JSON.parse(body).articles[3].title + 
+      "\n\nEnter 5 to read " + JSON.parse(body).articles[4].title, req, res)
+      url1 = JSON.parse(body).articles[0].url;
+      url2 = JSON.parse(body).articles[1].url;
+      url3 = JSON.parse(body).articles[2].url;
+      url4 = JSON.parse(body).articles[3].url;
+      url5 = JSON.parse(body).articles[4].url;
+      state = "news";
+    });
+
+}
+
+async function makeRequestWeather(uri, req, res) {
+  await request({
+    uri: uri,
+    method: "GET",
+    timeout: 10000,
+    followRedirect: true,
+    maxRedirects: 10
+  },
+    async function (error, response, body) {
+      sendMessage("Weather Type: " + JSON.parse(body).weather[0].main + 
+      "\n\nDescription: " + JSON.parse(body).weather[0].description + 
+      "\n\nTemperature: " + Math.round((JSON.parse(body).main.temp - 273.15) * 9.0 / 5 + 32) + " degrees Farenheit", req, res);
+    });
+}
+async function makeRequestArticle(url, res) {
+
+  console.log(url);
+  var req = unirest("GET", "https://lexper.p.rapidapi.com/v1.1/extract");
+
+  req.query({
+    "media": "1",
+    "url": url
+  });
+
+  req.headers({
+    "x-rapidapi-host": "lexper.p.rapidapi.com",
+    "x-rapidapi-key": "fcaaf11ba6mshddd853da7d73799p1a634djsnc1d151d36750"
+  });
+
+  let banana;
+  await req.end(async function (res) {
+    if (res.error) throw new Error(res.error);
+
+    banana = await res.body.article.text;
+    const accountSid = 'AC61eca8833f419fdc26e5ffa75b284891';
+    const authToken = '91bf82ff6ea981dfc77db8d5cb13ad4a';
+    const client = require('twilio')(accountSid, authToken);
+
+    sendMessage(banana, req, res);
+
+  });
+
+
+}

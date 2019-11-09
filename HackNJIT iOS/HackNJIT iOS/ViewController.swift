cc1e4146416bcb19dd0f44788b05213214a93767
Arya Tschand <arya@voicesaver.com>
2019-11-09 11:00:12 -0500

    Create iOS App Base

---
+++ HackNJIT iOS/HackNJIT iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackNJIT iOS
+//
+//  Created by Arya Tschand on 11/9/19.
+//  Copyright © 2019 Arya Tschand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

46212e794c6a601cb648b98ccda018abc677b561
Sai Vedagiri <43285226+SaiVedagiri@users.noreply.github.com>
2019-11-09 11:33:55 -0500

    Added node.js

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ express()
 
   // else
   // {
-    var URL = 'https://api.wolframalpha.com/v1/result?i=${input}%3F&appid=7PPG9H-H5KLEVRLK2';
-    $.getJSON(URL, function (data))
+    //var URL = 'https://api.wolframalpha.com/v1/result?i=${input}%3F&appid=7PPG9H-H5KLEVRLK2';
+    // $.getJSON(URL, function (data));
 
   // }
   /*

0c1330097757f6f14c2a463f4cbc6b81914aac00
Sai Vedagiri <43285226+SaiVedagiri@users.noreply.github.com>
2019-11-10 04:13:23 -0500

    Revert "revert"
    This reverts commit af409cadecaa57539cce6291a7e0bf705f75a2d9.

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var unirest = require("unirest");
 var screenshotmachine = require('screenshotmachine');
 
 const PORT = process.env.PORT || 5000
-const API_KEY = "AIzaSyDhkJ2yT06tRwXIMEUp9xaj2-LxOnKyvGY";
 
 const app = express();
 let state = "default";
@@ -22,6 +21,8 @@ let searchURL3;
 let searchURL4;
 let searchURL5;
 let fromNum;
+
+//getImage(`https://bing.com/search?q=banana&setlang=en-us&lf=1&cc=au`);
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
@@ -189,7 +190,7 @@ async function chatBot(input, currentFromNum, req, res) {
   fromNum = currentFromNum;
   input = input.trim();
   input = input.toLowerCase();
-  if (input.includes("init") || input.includes("home") || input.includes("exit") || input.includes("quit")) {
+  if (input.includes("init") || input.includes("home") || input.includes("exit")||input.includes("quit")) {
     state = "default";
     sendMessage(
       "Press a number corresponding to an action" +
@@ -221,48 +222,60 @@ async function chatBot(input, currentFromNum, req, res) {
     )
   }
 
-  else if (input.includes("2")) {
-    state = "inURL";
+  else if (state == "default" && input.includes("3")) {
+    state = "inInfo";
     sendMessage(
-      "Enter a url: ", req, res
+      "What do you want info about? ", req, res
     )
   }
-
-  else if (state = "inURL") {
-    if (!input.includes("http")) {
+  else if (state == "inInfo")
+  {
+    makeRequestWikipedia(input);
+  }
+  else if (state = "inURL")
+  {
+    if (!input.includes("http"))
+    {
       input = "https://" + input;
     }
+    sendMessage("Please wait...8")
     getImage(input);
     state = "default";
   }
 
   else if (state == "inSearch") {
-    sendMessage("Please wait...");
+    state = "inSearch2"
+    await sendMessage(`https://bing.com/search?q=${input}&setlang=en-us&lf=1&cc=au`);
+    await sendMessage("Please wait...");
     await getImage(`https://bing.com/search?q=${input}&setlang=en-us&lf=1&cc=au`);
     makeRequestSearch(input);
-    state = "inSearch2"
   }
 
-  else if (state = "inSearch2" && input.includes(1)) {
-    sendMessage("Please wait...");
+  else if (state == "inSearch2" && input.includes(1))
+  {
+    sendMessage("Please wait...2");
     await getImage(searchURL1);
   }
-  else if (state = "inSearch2" && input.includes(2)) {
-    sendMessage("Please wait...");
+  else if (state == "inSearch2" && input.includes(2))
+  {
+    sendMessage("Please wait...3");
     await getImage(searchURL2);
 
   }
-  else if (state = "inSearch2" && input.includes(3)) {
-    sendMessage("Please wait...");
+  else if (state == "inSearch2" && input.includes(3))
+  {
+    sendMessage("Please wait...4");
     await getImage(searchURL3);
 
   }
-  else if (state = "inSearch2" && input.includes(4)) {
-    sendMessage("Please wait...");
+  else if (state == "inSearch2" && input.includes(4))
+  {
+    sendMessage("Please wait...5");
     await getImage(searchURL4);
   }
-  else if (state = "inSearch2" && input.includes(5)) {
-    sendMessage("Please wait...");
+  else if (state == "inSearch2" && input.includes(5))
+  {
+    sendMessage("Please wait...6");
     await getImage(searchURL5);
   }
 }
@@ -460,10 +473,10 @@ async function makeRequestSearch(searchTerm, req, res) {
     searchURL4 = res.body.webPages.value[3].url;
     searchURL5 = res.body.webPages.value[4].url;
     sendMessage(
-      "\nPress 1 to view " + res.body.webPages.value[0].name +
-      "\n\nPress 2 to view " + res.body.webPages.value[1].name +
-      "\n\nPress 3 to view " + res.body.webPages.value[2].name +
-      "\n\nPress 4 to view " + res.body.webPages.value[3].name +
-      "\n\nPress 5 to view " + res.body.webPages.value[4].name, req, res)
+    "\nPress 1 to view " + res.body.webPages.value[0].name + 
+    "\n\nPress 2 to view " + res.body.webPages.value[1].name + 
+    "\n\nPress 3 to view " + res.body.webPages.value[2].name + 
+    "\n\nPress 4 to view " + res.body.webPages.value[3].name + 
+    "\n\nPress 5 to view " + res.body.webPages.value[4].name, req, res)
   });
 }
